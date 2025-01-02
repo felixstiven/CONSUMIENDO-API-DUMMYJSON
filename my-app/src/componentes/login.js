ec934c30c5867ba41eb575f0876caf1ce5655fc2
felixstiven <felixstiven12@gmail.com>
2025-01-02 15:31:01 -0500

    redirigiendo usuario despues de iniciar sesion

--- my-app/src/componentes/login.js
+++ my-app/src/componentes/login.js
@@ -3,11 +3,13 @@ import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import  Axios  from 'axios';
 import Swal from 'sweetalert2';
+import { useNavigate} from 'react-router-dom';
 
 function Login (){
 
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
+    const navigate = useNavigate(); // inicializando useHistory
 
     const submit = (e) => {
             e.preventDefault();
@@ -15,11 +17,17 @@ function Login (){
             Axios.post('http://localhost:3001/login', {
                 email,
                 password,
-            }).then(() => {
+            }).then(( response) => { // caprurando la respuesta 
+                const {token} = response.data;
+                localStorage.setItem('token', token);
+
                 Swal.fire({
                             title: " <h1>inicio de sesion exitoso!!</h1>",
                             icon: "success",
                             timer : 4000,
+                }).then(()=>{
+                    // redirigiendo despues de la alerta exito de sesion
+                    navigate('/create')
                 })
             }).catch((err) =>{
                 const errorMesagge = err.response && err.response.data && err.response.data.message
