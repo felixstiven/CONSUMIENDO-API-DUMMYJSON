8c8510556a8f5a8bee33521f1f313eea264811d5
felixstiven <felixstiven12@gmail.com>
2025-01-31 20:04:01 +0000

    agregando frotend y funcionalida contactar

--- consumiendo api/src/components/UserCard/UseCard.jsx
+++ consumiendo api/src/components/UserCard/UseCard.jsx
@@ -3,24 +3,24 @@ import { useState } from "react";
 export const UseCard = ({user}) => {
 
   const [isContacted, setIsContacted] = useState(false)
-  const [tecnologis, setTecnologis] = useState(['React', 'Node', 'MongoDB'])
+  //const [tecnologis, setTecnologis] = useState(['React', 'Node', 'MongoDB'])
 
   const {id, firstName, email, image} = user
 
   const handleClick = () => {
-    const nexTecnologis = [...tecnologis, 'Express']
-    setTecnologis(nexTecnologis)
-}
+    setIsContacted(!isContacted)
+    
+  }
 
 
   return (
     <div className='card' >
         <img  className="image" src={image} alt={firstName}/>
         <h2 className='name'>{firstName}</h2>
-        <p className='description'>{email}</p>
+        <p className='email'>{email}</p>
         <button  id={id} onClick={() => {handleClick()}}>
           {
-            isContacted ? 'Contactado' : 'Contactar'
+            isContacted ? '3107729036' : 'Contactar'
           }
         </button>
     </div>
