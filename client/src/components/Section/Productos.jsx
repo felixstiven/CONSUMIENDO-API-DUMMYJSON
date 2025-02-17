0fe10fd94185fde8706e0e23ab315a99f47989fb
felixstiven <felixstiven12@gmail.com>
2025-02-17 19:31:50 +0000

    agregando logica (fucionalidad) mostrar y ocultar productos

--- client/src/components/Section/Productos.jsx
+++ client/src/components/Section/Productos.jsx
@@ -2,27 +2,58 @@ import './Productos.css';
 import { useState, useEffect } from 'react';
 import { Header } from '../Header/Header';
 import { ProductosCard } from '../ProductCard/ProductCard';
+import { use } from 'react';
+
+
+
+
 
 
 
 export const Productos = () => {
     const [productos, setProductos] = useState([]);
+    const [estado, setEstado] = useState(false);
+    const [ocultar, setOcultar] = useState("Obtener Todos los productos de la Api");
 
-    useEffect(()=>{
-        fetch(`https://dummyjson.com/products`)
-        .then(res => res.json())
-        .then(data=>{
-            console.log(data)
-            setProductos(data.products) 
-        })
-    },[])
+
+
+        
+            useEffect(()=>{
+                fetch(`https://dummyjson.com/products`)
+                .then(res => res.json())
+                .then(data=>{
+                    console.log(data)
+                    setProductos(data.products) 
+                })
+            },[])
+   
+   
+
+    const handleClick = () => {
+        setEstado (!estado);
+
+        setOcultar( estado ? "Ver todos Productos" : "Ocultar productos");
+    }
 
     return (
         <> 
             <Header title="Productos"/>
+            <section>
+                <div>
+                    <h1>Productos de DummyJson</h1>
+                    <div>
+                        <p>Aplicacion frotend apuntando a uno de los endpoint de la apy DummyJson, donde capturamos los productos en formato Json y renderizamos el cliente en componentes del lado del cliente.</p>
+                         <button onClick={handleClick}>{ ocultar}</button>
+                    </div>
+                </div>
+            </section>
             {
                 productos && productos.map((producto)=>{
-                    return <section key={producto.id}><ProductosCard contenido={producto}/></section>
+                    if (estado === true){
+                        return (
+                            <section key={producto.id}><ProductosCard contenido={producto}/></section>
+                        ) 
+                    }
                     
                 })
            }
