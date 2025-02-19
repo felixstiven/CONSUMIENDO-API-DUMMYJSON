c3bbe19c0f650287fbb2be313ba32c2201a74cb7
felixstiven <felixstiven12@gmail.com>
2025-02-19 18:46:39 -0500

    udpdate logic whit javascript for components of products and styles css

--- client/src/components/ProductCard/ProductCard.jsx
+++ client/src/components/ProductCard/ProductCard.jsx
@@ -1,14 +1,12 @@
+import './ProductosCard.css';
 
-
-
-
-
-export const ProductosCard = ({ contenido }) => {
-    const { _id, description, price, images } = contenido;
+export const ProductosCard = ({contenido}) => {
+    const {id, description, price, images } = contenido;
+  
 
     return (
         <>
-          <div   className='card' id={_id}>
+          <div   className='card' key={id} id={id}>
             <img className="image" src={images[0]} alt={description}/>
             <h2 className='description'>{description}</h2>
             <div className="price-container">
--- client/src/components/Section/Productos.jsx
+++ client/src/components/Section/Productos.jsx
@@ -2,10 +2,6 @@ import './Productos.css';
 import { useState, useEffect } from 'react';
 import { Header } from '../Header/Header';
 import { ProductosCard } from '../ProductCard/ProductCard';
-import { use } from 'react';
-
-
-
 
 
 
@@ -14,49 +10,69 @@ export const Productos = () => {
     const [productos, setProductos] = useState([]);
     const [estado, setEstado] = useState(false);
     const [ocultar, setOcultar] = useState("Obtener Todos los productos de la Api");
+    const [count, setCount] = useState(1)
 
+    useEffect(()=>{
+        fetch(`https://dummyjson.com/products/${count}`)
+        .then(res => res.json())
+        .then(data=>{
+            console.log(data)
+            if (data && data.id){
+                setProductos(data)
+            } else{
+                alert('no se encontraron productos')
+            }
+        })
+    },[count]);
 
+    const handleAnterior = () =>{
+        if (count > 1 ){
+            setCount( count - 1)
+        } else{
+            alert('no se puede dar para atras')
+        }
+    };
 
-        
-            useEffect(()=>{
-                fetch(`https://dummyjson.com/products`)
-                .then(res => res.json())
-                .then(data=>{
-                    console.log(data)
-                    setProductos(data.products) 
-                })
-            },[])
-   
-   
+    const handleSiguiente = () =>{
+        setCount( count +1 )    
+    };
 
     const handleClick = () => {
         setEstado (!estado);
 
         setOcultar( estado ? "Ver todos Productos" : "Ocultar productos");
-    }
+    };
 
     return (
         <> 
             <Header title="Productos"/>
-            <section>
-                <div>
-                    <h1>Productos de DummyJson</h1>
-                    <div>
-                        <p>Aplicacion frotend apuntando a uno de los endpoint de la apy DummyJson, donde capturamos los productos en formato Json y renderizamos el cliente en componentes del lado del cliente.</p>
-                         <button onClick={handleClick}>{ ocultar}</button>
+            <section className='container-princip'>
+                    <div className='container-title'>
+                        <h1>Productos de DummyJson</h1>
+                        <div className=''>
+                            <p>Aplicacion frotend apuntando a uno de los endpoint de la apy DummyJson, donde capturamos los productos en formato Json y renderizamos el cliente en componentes del lado del cliente.</p>
+                            <button onClick={handleClick}>{ ocultar}</button>
+                        </div>
                     </div>
-                </div>
+                {
+                    estado ? (
+                        <>
+                            <div className='container-card'>
+                                <ProductosCard key={productos.id} contenido={productos} />
+                                <div className='container-button'>
+                                    <button onClick={handleAnterior}>Anterior</button>
+                                    <button onClick={handleSiguiente}>siguiente</button>
+                                </div>
+                            </div>
+                        </>
+                    ) : (
+                        <div>
+                            <img src="api.png" alt="imagen de productos" />
+                        </div>
+                    )
+                }    
+
             </section>
-            {
-                productos && productos.map((producto)=>{
-                    if (estado === true){
-                        return (
-                            <section key={producto.id}><ProductosCard contenido={producto}/></section>
-                        ) 
-                    }
-                    
-                })
-           }
         </>
     )
 }                               
