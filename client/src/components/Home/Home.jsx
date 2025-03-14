7aac650641ad4bff84ba4ee20fa72d573f383390
felixstiven <felixstiven12@gmail.com>
2025-03-14 18:10:23 -0500

    adding styles in components

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -2,6 +2,8 @@
 import { Header } from "../Header/Header.jsx";
 import { GiTargetDummy } from "react-icons/gi";
 import { FaServer } from "react-icons/fa";
+import { IoLogoWhatsapp, IoMdMail } from "react-icons/io";
+import { FaGithub } from "react-icons/fa6";
 
 
 
@@ -26,28 +28,30 @@ function Home() {
                     <div className="fila">
                         <div className="col">
                             <GiTargetDummy className="fa-user"/>
-                            <span className="titulo-detalle">APY DUMMIJSON</span>
+                            <h4 className="titulo-detalle">APY DUMMIJSON</h4>
                             <p>Solicite a la api y obtenga datos de productos y usuarios</p>
-                            <div>
-                                <button className="btn btn-primary">Jugar</button>
+                            <div className="container-bt">
+                                <button className="bt">Jugar</button>
                             </div>
                         </div>
                         <div className="col">
                             <FaServer className="fa-user"/>
-                            <span className="titulo-detalle">MY API RESTful</span>
+                            <h4 className="titulo-detalle">MY API RESTful</h4>
                             <p>Ve y comprueba realizando solicitudes http de mi propio servidor</p>
-                            <div>
-                                <button className="btn btn-primary">Jugar</button>
+                            <div className="container-bt">
+                                <button className="bt">Jugar</button>
                             </div>
                         </div>
                         
                     </div>
                 </div>
             </section> 
-            <section className="sobremi" id="sobremi">
+            <section className="sobremi sobremi-2" id="sobremi">
                 <div className="contenido-seccion seccion-2">
-                    <h2 className="titulo-seccion-2">Ganador hackathon TECH 2024</h2>
-                    <p className="especial especial-2">Desarollo de una aplicacion web para la Unidad Administrativa Especial de    Servicios Publicos (UAESP)</p>
+                    <div className="titulo-principal">  
+                        <h2 className="titulo-seccion-2">Ganador hackathon TECH 2024</h2>
+                        <p className="especial especial-2">Desarollo de una aplicacion web para la Unidad Administrativa Especial de    Servicios Publicos (UAESP)</p>
+                    </div>
                     <div className="container-hackathon">
                         <div className="hackathon">
                             <img src="hackathon.jpeg" alt="UAESP" /> 
@@ -59,20 +63,21 @@ function Home() {
                 </div>
             </section> 
             <section className="contacto" id="contacto">
-                <div className="contenido-seccion">
-                    <h2 className="titulo-seccion">Contacto</h2>
-                    <h3>Ponte en contacto ahora mismo!</h3>
-                    <div className="fila">
+                    <div className="contenido-seccion">
+                        <h3>CONTACTO</h3>
+                        <p>Ponte en contacto ahora mismo</p>
+                    </div>
+                     <div className="fila">
                         <div className="col">
-                            <h2>Telefono</h2>
-                            <a className="info-contacto" href="https://api.whatsapp.com/send/?phone=573107729036">+573107729036</a>
+                            <a className="info-contacto" href="https://api.whatsapp.com/send/?phone=573107729036"><IoLogoWhatsapp  /></a>
                         </div>
                         <div className="col">
-                            <h2>Correo</h2>
-                            <a className="info-contacto" href="mailto:felixstiven12@gmail.com">felixstiven12@gmail.com</a>
+                            <a className="info-contacto" href="mailto:felixstiven12@gmail.com"><IoMdMail /></a>
+                        </div>
+                        <div className="col">
+                            <a className="info-contacto" href="https://github.com/felixstiven"><FaGithub /></a>
                         </div>
                     </div>
-                </div>
             </section>
       </>
   );
